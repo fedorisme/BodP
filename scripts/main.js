07e06cf004a2b004486a581367eacb029363304d
fedorisme <78550668+fedorisme@users.noreply.github.com>
2021-02-08 13:37:07 +0100

    added artist page

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,36 @@
 // Template file
 // Author: Fedor
 
+var element = document.getElementById('navlogo');
+var bodyRect = document.body.getBoundingClientRect();
+    elemRect = element.getBoundingClientRect();
+    offset   = elemRect.top - bodyRect.top;
+
 document.onscroll = function(){
 
-	if (window.pageYOffset > 499) {
+	if (window.pageYOffset > (offset - 8)) {
 		console.log("hamster");
 		document.getElementById('navlogo').classList.add("showlogo");
-	};
+	} else {
+		document.getElementById('navlogo').classList.remove("showlogo");
+	}
 };
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+      // Prevent default anchor click behavior
+        event.preventDefault();
+        // Store hash
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
