eca6a95e61cb57412f59f44ce01e859431ca2627
fedorisme <78550668+fedorisme@users.noreply.github.com>
2021-02-08 11:06:36 +0100

    JS update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 // Template file
 // Author: Fedor
 
+document.onscroll = function(){
+
+	if (window.pageYOffset > 499) {
+		console.log("hamster");
+		document.getElementById('navlogo').classList.add("showlogo");
+	};
+};
